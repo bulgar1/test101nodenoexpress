291d4d718c57e5ac9e421c044c689e4906f603c9
bulgar1 <boraexpress@yahoo.com>
2016-04-21 06:10:56 -0700

    h1

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ var path = require('path');
  app.get('/', function(req, res) {
  res.sendFile(path.join(__dirname + '/index.html'));
  });
+// create routes for the admin section 2
+ // get an instance of the router
+ var adminRouter = express.Router();
+
+// admin main page. the dashboard (http://localhost:1337/admin)
+ adminRouter.get('/', function(req, res) {
+ res.send('I am the dashboard!');
+ });
+
+// users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
+res.send('I show all the users!');
+});
+
+// posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+ res.send('I show all the posts!');
+});
 
 // start the server
  app.listen(1337);
